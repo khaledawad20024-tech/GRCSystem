407b76bbe79dc2303c243e9aecb6acad09a903d4
khaledawad20024-tech <khaledawad20024@gmail.com>
2026-06-18 21:28:49 +0300

    Add IUnitOfWork interface

---
+++ Backend/GRC.Application/Services/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+namespace GRC.Application.Services
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
+}
